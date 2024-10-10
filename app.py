59f76101c2d6102dbf7a1b7064c7c677e899b0cf
ismaelVie <ismael.viennot@gmail.com>
2024-10-10 09:27:31 +0200

    Add age calculation feature

--- app.py
+++ app.py
@@ -8,9 +8,18 @@ def index():
     if request.method == 'POST':
         name = request.form['name']
         dob = request.form['dob']
-        welcome_message = f"Welcome, {name}! Your date of birth is {dob}."
+        age = calculate_age(dob)
+        welcome_message = f"Welcome, {name}! You are {age} years old."
         return render_template('result.html', message=welcome_message)
     return render_template('index.html')
+                           
+
+def calculate_age(dob):
+    today = datetime.today()
+    birth_date = datetime.strptime(dob, "%Y-%m-%d")
+    age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+    return age
+
 
 if __name__ == '__main__':
     app.run(debug=True)
