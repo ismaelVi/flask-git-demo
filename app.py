bd4a2ead2f842d2c5a876da7df19c5393d6de68c
ismaelVie <ismael.viennot@gmail.com>
2024-10-10 10:05:25 +0200

    Add birthday countdown feature

--- app.py
+++ app.py
@@ -22,6 +22,14 @@ def calculate_age(dob):
     return age
         
 
+def days_to_birthday(dob):
+    today = date.today()
+    dob = datetime.strptime(dob, "%Y-%m-%d").date()
+    next_birthday = date(today.year, dob.month, dob.day)
+    if next_birthday < today:
+        next_birthday = date(today.year + 1, dob.month, dob.day)
+    return (next_birthday - today).days
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -29,7 +37,8 @@ def index():
         dob = request.form['dob']
         age = calculate_age(dob)
         zodiac = get_zodiac_sign(dob)
-        message = f"Welcome, {name}! You are {age} years old. Your zodiac sign is {zodiac}."
+        days_to_bday = days_to_birthday(dob)
+        message = f"Welcome, {name}! You are {age} years old. Your zodiac sign is {zodiac}. There are {days_to_bday} days until your next birthday!"
         return render_template('result.html', message=message)
     return render_template('index.html')
                            
